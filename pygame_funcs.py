c4a7feea41da1a5b57d998228401a6783dedd4b7
Logan <88503173+Mistah6909@users.noreply.github.com>
2022-05-24 17:59:49 -0500

    Update pygame_funcs.py

--- pygame_funcs.py
+++ pygame_funcs.py
@@ -1,1 +1,8 @@
+import pygame
 
+class entities(pygame.sprite.Sprite):
+  def __init__(self,pos,image):
+    super().__init__()
+    self.image = image
+    self.image =  pygame.transform.scale(pos,image)
+    self.rect = self.image.get_rect()
